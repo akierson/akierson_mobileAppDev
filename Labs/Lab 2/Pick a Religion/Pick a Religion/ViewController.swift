5cc997b7119a2d00099ea6f6b8f663e381ef86bd
A Kierson <azki9459@colorado.edu>
2018-09-18 12:44:17 -0600

    Initial Coding for Lab 2 added

--- Labs/Lab 2/Pick a Religion/Pick a Religion/ViewController.swift
+++ Labs/Lab 2/Pick a Religion/Pick a Religion/ViewController.swift
@@ -13,17 +13,143 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textBox: UITextView!
     // Control Outlets
-    @IBOutlet weak var pcButton: UIBarButtonItem!
-    @IBOutlet weak var religionPicker: UISegmentedControl!
-    @IBOutlet weak var religionLevel: UISlider!
+    @IBOutlet weak var pgSwitch: UISwitch!
+    @IBOutlet weak var fruitLevel: UISlider!
+    @IBOutlet weak var fruitPicker: UISegmentedControl!
     
     // Helper functions
     func updateImage() {
-        if religionLevel.value < 5 {
-            imageView.image = UIImage(named: <#T##String#>)
+        let f_level = Int(fruitLevel.value/3.0)
+        let f_type = fruitPicker.selectedSegmentIndex
+        switch f_type {
+        case 0:
+            imageView.image = UIImage(named: "red_" + String( f_level))
+        case 1:
+            imageView.image = UIImage(named: "yellow_" + String( f_level))
+        case 2:
+            imageView.image = UIImage(named: "blue_" + String( f_level))
+        case 3:
+            imageView.image = UIImage(named: "green_" + String( f_level))
+        default:
+            print("Segmented control out of range")
         }
     }
+    
     func updateText() {
+        let expletive = pgSwitch.isOn ? "" : "fucking"
+        let f_level = fruitLevel.value
+        let f_type = fruitPicker.selectedSegmentIndex
+        
+        // Switch case for different fruits
+        switch f_type {
+        // Apples
+        case 0:
+            switch f_level{
+            case 0...4:
+                textBox.text = """
+                Base Level Cultist
+                + Speed:
+                + Money:
+                - Like Scientologists but worse
+                """
+            case 5...7:
+                textBox.text = """
+                Tech Reviewer Cultist
+                + Acquire speed:
+                + Money:
+                - Empathy:
+                """
+            case 8...:
+                textBox.text = """
+                Apple Overlord
+                + V̅̌ͣ͂̀̓ͦͨ̌̇ͩͪ̃Ë̄͑̄ͥ̐ͩͦͣͮ̊͗ͤͮ͆̋͌͑
+                + T̆̍ͤ͌̍ͬ̂͊̈́̂̊͋Eͤ͗̃̒͒ͩV̊̎̈͌͋ͩ̌ͭ͒̅ͣ̐́̄̍ͧ̽̈́
+                - S̜̣͉̯̞̖̖̥̫̠̠̦̗̓̓ͮ̈́̃͋̏ͧͨ̓̂̎̉̅T͍̞̱̫̗̘̼̩̭̄̄ͬ͗̔ͥ̄͐͊̃̿͊͒̏ͧͪ̓̉ͅĔ̻̪̝̬͈̼͍̥̍͆͆́ͣͩͨ̔ͥ̂͆ͯ́ͩ́
+                """
+            default:
+                print("Level Control out of range")
+            }
+        //Bananas
+        case 1:
+            switch f_level{
+            case 0...4:
+                textBox.text = """
+                Split
+                + Eyepatch
+                + Brownness: \(f_level)
+                - Phallic
+                """
+            case 5...7:
+                textBox.text = """
+                Rambonana
+                + Two Eyepatches
+                + Brownness: \(f_level)
+                + Really Cool
+                """
+            case 8...:
+                textBox.text = """
+                Predator Naner
+                + Dreads
+                + Brownness: \(f_level)
+                + Like Really, really cool
+                """
+            default:
+                print("Level Control out of range")
+            }
+        // Blue Berries
+        case 2:
+            switch f_level{
+            case 0...4:
+                textBox.text = """
+                Bluetallica
+                + Base Tier coolness
+                +
+                """
+            case 5...7:
+                textBox.text = """
+                Blue Maiden
+                +
+                """
+            case 8...:
+                textBox.text = """
+                Nowegian Blue Metal Band
+                + Probably  Dead
+                + Is part of the forest
+                - Inacessible to most people
+                """
+            default:
+                print("Level Control out of range")
+            }
+        // Avocado
+        case 3:
+            switch f_level{
+            case 0...4:
+                textBox.text = """
+                Split
+                + Eyepatch
+                + Brownness:
+                - Phallic
+                """
+            case 5...7:
+                textBox.text = """
+                Rambonana
+                + Two Eyepatches
+                + Brownness
+                + Really Cool
+                """
+            case 8...:
+                textBox.text = """
+                Predator Naner
+                + Dreads
+                + Brownness
+                + Like Really, really cool
+                """
+            default:
+                print("Level Control out of range")
+            }
+        default:
+            break
+        }
         // update some stats based on level
         // update color by religion and level
         // all start black go to (yellow, blue, black)
@@ -35,13 +161,7 @@ class ViewController: UIViewController {
     @IBAction func pcButtonFunc(_ sender: UIBarButtonItem) {
         // Disable all options and set text to "You are not prepared"?
     }
-    
-    @IBAction func religionPickerFunc(_ sender: UISegmentedControl) {
-        updateImage()
-        updateText()
-    }
-    
-    @IBAction func religionLvlFunc(_ sender: UISlider) {
+    @IBAction func fruitPickerFunc(_ sender: UISegmentedControl) {
         updateImage()
         updateText()
     }
